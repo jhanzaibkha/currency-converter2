8c13975d125337227f15cecdf3853f0bafe88763
jhanzaibkha <164366947+jhanzaibkha@users.noreply.github.com>
2024-05-23 08:48:38 -0700

    Adding a CLI Simple currency Converter project

---
+++ main.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+let exchange_rate: any = {
+    "USD":1,//Base Currency
+    "EUR":0.9,//European Currency
+    "JYP":113,//Japanese Currency
+    "CAD":1.3,//Canadian Currency
+    "AUD":1.65,//Australian Dollar
+    "PKR":278.45, //Pakistani Rupees
+}
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency from: ",
+        choices: ["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: ["USD","EUR","JYP","CAD","AUD","PKR"]
+},
+{
+    name: "amount",
+    type: "input",
+    message: "Enter the amount to convert"
+}
+]);
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+//formula of conversion 
+let base_amount = amount/from_amount
+let converted_amount = base_amount * to_amount
+//Display the converted amount
+console.log(`Converted Amount = ${converted_amount.toFixed(2)}`);
+
+
+
